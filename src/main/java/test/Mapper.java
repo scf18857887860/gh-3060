183bc90ce9dabdaa1254edd62777d275162bd63e
1107156537@qq.com <1107156537@qq.com>
2024-01-18 09:36:03 +0800

    test seelctError

--- src/main/java/test/Mapper.java
+++ src/main/java/test/Mapper.java
@@ -1,7 +1,15 @@
 package test;
 
+import cn.hutool.json.JSONObject;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
 public interface Mapper {
   User getUser(Integer id);
 
   void insertUser(User user);
+
+
+  List<LinkedHashMap<String, ?>> queryListByDynamicSql(JSONObject params);
 }
--- src/test/java/test/SimpleTest.java
+++ src/test/java/test/SimpleTest.java
@@ -1,10 +1,6 @@
 package test;
 
-import static org.junit.Assert.*;
-
-import java.io.Reader;
-import java.sql.Connection;
-
+import cn.hutool.json.JSONObject;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.apache.ibatis.session.SqlSession;
@@ -12,6 +8,13 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.Reader;
+import java.sql.Connection;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class SimpleTest {
 
@@ -56,6 +59,27 @@ public class SimpleTest {
       Mapper mapper = sqlSession.getMapper(Mapper.class);
       User user = mapper.getUser(2);
       assertEquals("User2", user.getName());
+    }
+  }
+
+
+  @Test
+  public void selectError() {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      Mapper mapper = sqlSession.getMapper(Mapper.class);
+      User user = new User();
+      user.setId(2);
+      user.setName("111[张三]");
+      mapper.insertUser(user);
+      sqlSession.commit();
+    }
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      Mapper mapper = sqlSession.getMapper(Mapper.class);
+      String sql="select case when name='aaa[张三]' then 1 else 0 end as 'aaa[张三]' from users limit 1" ;
+      JSONObject jsonObject=new JSONObject();
+      jsonObject.putIfAbsent("__sql__",sql);
+      List<LinkedHashMap<String, ?>> linkedHashMaps = mapper.queryListByDynamicSql(jsonObject);
+      System.out.println(linkedHashMaps.toArray());
     }
   }
 
